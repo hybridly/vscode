bb7ffc28eb462ec5f2c7d329ae60e9d2deab5c5e
Enzo Innocenzi <enzo@innocenzi.dev>
2023-01-17 20:22:25 +0100

    feat: support PSR-4 loading

---
+++ src/utils/psr4.ts
@@ -0,0 +1,59 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import { Uri } from 'vscode'
+import { log } from './log'
+
+interface Autoload {
+	'psr-4': Record<string, string>
+}
+
+interface ControllerAction {
+	fqcn: string
+	file: string
+	action: string
+	uri: Uri
+}
+
+export function getComposerAutoload(workspace: Uri): Autoload {
+	const composer = JSON.parse(fs.readFileSync(path.resolve(workspace.fsPath, 'composer.json'), { encoding: 'utf-8' }))
+
+	return composer.autoload
+}
+
+export function fqcnToFile(workspace: Uri, fqcn: string): string {
+	const autoload = getComposerAutoload(workspace)
+
+	fqcn = fqcn.replaceAll('\\\\', '\\')
+
+	Object.entries(autoload['psr-4']).forEach(([key, value]) => {
+		if (fqcn.startsWith(key)) {
+			fqcn = fqcn.replace(key, value)
+		}
+	})
+
+	return `${fqcn.replaceAll('\\', '/')}.php`
+}
+
+export function actionToLink(workspace: Uri, action: string): ControllerAction | undefined {
+	const [fqcn, actionName = '__invoke'] = action.split('@')
+	const file = fqcnToFile(workspace, fqcn)
+	const uri = Uri.joinPath(workspace, file)
+
+	if (!fs.existsSync(uri.fsPath)) {
+		log.appendLine(`Could not find controller: ${uri.fsPath}`)
+		return
+	}
+
+	const lines = fs.readFileSync(uri.fsPath).toString().split('\n')
+	const lineIndexOfAction = lines.findIndex((line) => line.includes(`function ${actionName}`)) + 1
+	const column = (lines.at(lineIndexOfAction)?.length ?? 0)
+
+	return {
+		fqcn,
+		file,
+		action: actionName,
+		uri: uri.with({
+			fragment: `L${lineIndexOfAction + 1},${column + 1}`,
+		}),
+	}
+}
