3467a9c8d4855265558d1ff27a3a945bbfa1b3e0
Enzo Innocenzi <enzo@innocenzi.dev>
2023-12-21 03:23:37 +0100

    fix(tests): do not require being in a php file to run all tests

--- src/commands/run-tests.ts
+++ src/commands/run-tests.ts
@@ -1,11 +1,11 @@
 /* eslint-disable brace-style */
 import { ExtensionContext } from '../context'
-import { registerPhpFileCommand } from '../utils/commands'
+import { registerEditorCommand } from '../utils/commands'
 import { hasPest } from '../utils/composer'
 import { runTestsTask } from '../utils/tests'
 
 export async function registerRunTestsCommand(context: ExtensionContext) {
-	registerPhpFileCommand(context, 'run-tests', async() => {
+	registerEditorCommand(context, 'php.run-tests', async() => {
 		if (!hasPest(context.cwd)) {
 			return
 		}
