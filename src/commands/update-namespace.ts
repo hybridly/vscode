54e2641aa1ba3fe5db93f8a8d69f41b47ed3f217
Enzo Innocenzi <enzo@innocenzi.dev>
2025-03-19 23:32:41 +0100

    feat(update-namespace): also update class declaration to PSR-4

--- src/commands/update-namespace.ts
+++ src/commands/update-namespace.ts
@@ -1,7 +1,10 @@
+import type { TextEditor, TextLine } from 'vscode'
 import { Position, Range } from 'vscode'
 import type { ExtensionContext } from '../context'
 import { registerPhpFileCommand } from '../utils/commands'
 import { generatePhpPrelude } from '../utils/generate-php-class'
+
+const classDeclarationRE = /class ([a-zA-Z_\x7F-\xFF][a-zA-Z0-9_\x7F-\xFF]*)/
 
 export async function registerUpdateNamespaceCommand(context: ExtensionContext) {
 	registerPhpFileCommand(context, 'update-namespace', async ({ file, editor }) => {
@@ -9,8 +12,17 @@ export async function registerUpdateNamespaceCommand(context: ExtensionContext) 
 			new Range(new Position(0, 0), new Position(6, 0)),
 		).match(/namespace [^;]+;/)
 
-		const documentText = editor.document.getText()
+		const currentlySpecifiedClassName = findClassDeclarationLine(editor)
+		let documentText = editor.document.getText()
 		const newDocumentText = (() => {
+			// Replaces the class declaration
+			if (currentlySpecifiedClassName) {
+				const line = currentlySpecifiedClassName.lineNumber
+				const lineText = editor.document.lineAt(line).text
+
+				documentText = documentText.replace(lineText, lineText.replace(classDeclarationRE, `class ${file.className}`))
+			}
+
 			// Replaces the existing namespace if any
 			if (currentlySpecifiedNamespace) {
 				return documentText.replace(currentlySpecifiedNamespace[0], `namespace ${file.fqcn};`)
@@ -34,4 +46,23 @@ export async function registerUpdateNamespaceCommand(context: ExtensionContext) 
 			)
 		})
 	})
+}
+
+function findClassDeclarationLine(editor: TextEditor): TextLine | undefined {
+	let line = 0
+	let previousLineIsClassDeclaration = false
+	const lines = editor.document.lineCount
+
+	while (line < lines) {
+		const lineText = editor.document.lineAt(line).text
+		const classOpenerRE = /^{$/
+
+		if (previousLineIsClassDeclaration && lineText.match(classOpenerRE)) {
+			return editor.document.lineAt(line - 1)
+		}
+
+		previousLineIsClassDeclaration = classDeclarationRE.test(lineText)
+
+		line++
+	}
 }
